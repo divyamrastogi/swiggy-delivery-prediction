af2a195164f671468eddecedad050061ececd036
Chirag Jain <chiragj@uber.com>
2018-07-14 15:19:50 +0530

    add Map component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Map from './components/Map.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Map,
   },
 };
 </script>
---
+++ src/components/Map.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="map">
+  </div>
+</template>
+
+<script>
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpcmdqbiIsImEiOiJjampsNjVmYTYyNGhnM3JteG82ejJyNzNhIn0.e2zSwRb06-oK5pE4vjVK1A';
+
+export default {
+  mounted() {
+    console.log(this.$el);
+    mapboxgl.Map({
+      container: this.$el,
+      style: 'mapbox://styles/mapbox/streets-v10',
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
