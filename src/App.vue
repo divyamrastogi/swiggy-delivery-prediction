b10357463de7b6cb44930825002a95a756d929b8
Divyam Rastogi <divyamr@flock.com>
2018-07-14 18:41:35 +0530

    Specify light theme for mapbox.

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@ import Components from '@flockos/vue-components';
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 
 const { L } = window;
-const accessToken = 'pk.eyJ1IjoiZGl2eWFtc3VwZXJiIiwiYSI6ImNpeXY1MGdtNDAwMHYzMnFoeG9sbW5vN2gifQ.NjxieTUN_mNsyRv7NjUWCw';
 export default {
   name: 'app',
   components: {
@@ -29,7 +28,7 @@ export default {
       bannerMsg: '',
       map: null,
       zoom: 13,
-      url: `https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${accessToken}`,
+      url: `https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${process.env.VUE_APP_MAPBOX_KEY}`,
       center: L.latLng(0, 0),
     };
   },
